8f4d319fc79b368babd450c388de3266759aa279
Alome Daniel <47638856+alome007@users.noreply.github.com>
2021-07-15 09:20:28 +0100

    Update Constants.kt

--- app/src/main/java/com/alome/mvp/Misc/Constants.kt
+++ app/src/main/java/com/alome/mvp/Misc/Constants.kt
@@ -3,7 +3,7 @@ package com.alome.mvp.Misc
 class Constants {
     companion object {
         const val POSTER_BASE_URL ="https://image.tmdb.org/t/p/w185/"
-        const val MOVIE_DB_BASE_API_KEY ="87ee6c06354ad238fe5f821f47b3310c"
+        const val MOVIE_DB_BASE_API_KEY ="YOUR_MOVIEDB_API_KEY"
         const val MOVIE_DB_BASE_URL ="https://api.themoviedb.org/"
         const val BLACKLISTED_MOVIES = "black_listed_movies"
         const val MOVIE = "movie"
